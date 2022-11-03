2690996731f5a0ea7b632a1769e9fe38d5c1bce1
[Students] Kirils Ivanovs <01DPKIvanovs@rvt.lv>
2022-11-03 11:37:19 +0200

    new video

--- login.js
+++ login.js
@@ -1,10 +1,33 @@
-// Below function Executes on click of login button.
 function Login(){
-var username = document.getElementById("username").value;
-var password = document.getElementById("password").value;
-if ( username == "Formget" && password == "formget#123"){
-alert ("Login successfully");
-window.location = "index.html"; // Redirecting to other page.
-return false;
-}
+    
+
+    login_name = document.getElementById('input#username').value;
+
+    login_pass = document.getElementById('input#password').value;
+
+    console.log(login_name)
+    console.log(login_pass)
+
+    if(login_name === "admin" && login_pass == "admin"){
+
+        window.location.href = "game.html";
+    }
+    /*for(let i = 0; i < user_data.length; i++) {
+       
+        
+
+        if(login_name === user_data[i].username && login_pass == user_data[i].password){
+
+            window.location.href = "game.html";
+            let current_user_name = localStorage.setItem("Current User Name", login_name)
+        } 
+
+        
+
+
+    }*/
+
+    username.value = "";
+    password.value = "";
+
 }
